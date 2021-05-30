d382609e47d15d63fdb7949f52b10c675ad40b90
tbinhussain <81320512+tbinhussain@users.noreply.github.com>
2021-05-30 14:04:38 +0300

    i fixed the bags.php page

--- tala/bag.php
+++ tala/bag.php
@@ -1,11 +1,21 @@
+
+<head>
+    <meta charset="UTF-8">
+    <title>Purple Zipper Bags</title>
+
+    <link rel="stylesheet" href="css.css">
+</head>
+<!-- <div class="small-container"> -->
+    <h2 class="title">Bag collection </h2>
 <?php
 $con = mysqli_connect("localhost","root","","purple_z");
 if(!$con){
     die("Connection failed". mysqli_connect_error());
-    $qry= "SELECT* FROM product WHERE PROD_TYPE='Bag'";
+}
+    $qry= "SELECT * FROM product WHERE PROD_TYPE='Bag'";
     $rst=mysqli_query($con,$qry);
 
-    While($row-mysqli_fetch_assoc($rst)){
+    While($row=mysqli_fetch_assoc($rst)){
         $prod_ID=$row["PROD_ID"];
         $prod_NAME=$row["PROD_NAME"];
         $prod_TYPE=$row["PROD_TYPE"];
@@ -13,14 +23,25 @@ if(!$con){
         $prod_DESC=$row["PROD_DESC"];
         $prod_STARS=$row["PROD_STARS"];
         $prod_PRICE=$row["PROD_PRICE"];
-    }
-}
+
+
 
             ?>
+            <!--
 <img src="<?php echo $prod_IMG?>">
 <h1><?php echo $prod_NAME?></h1>
 <p><?php echo $prod_DESC?></p>
-<p class="price"><?php echo $prod_PRICE ?> </p>
+<p class="price"><?php echo $prod_PRICE ?> </p> -->
 
 
-<?php
+            <div class="col-4" style="padding-bottom: 25px">
+                <img width="150px" height="350px" src=".././<?php echo $prod_IMG; ?>">
+                <h4><?php echo $prod_NAME; ?></h4>
+                <p><?php echo $prod_DESC; ?></p>
+                <p><?php echo $prod_PRICE. " KD" ?></p>
+            </div>
+
+
+
+<?php } ?>
+<!--</div> -->
