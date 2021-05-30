e6017bad54c6137107ded38d6ab1eb1673f67e49
Mustafa-Karkour <Mustafa-Karkour@users.noreply.github.com>
2021-05-30 12:29:00 +0300

    fixed some issues in invoice.php file

--- phps/invoice.php
+++ phps/invoice.php
@@ -78,7 +78,11 @@ unset($_SESSION["myCart"]);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     
-   
+   <!--Font Awesome-->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+    />
     <!-- Bootstrap -->
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
