29bb24ef35c91522702039ef1d6ad276b3183107
Zahraa <S00050748@auk.edu.kw>
2021-05-30 15:37:37 +0300

    Updated

--- Zahraa/All/dbh.php
+++ Zahraa/All/dbh.php
@@ -1,23 +1,34 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Purple Zipper Bags</title>
+
+    <link rel="stylesheet" href="css.css">
+</head>
+<!-- <div class="small-container"> -->
+<h2 class="title">Bag collection </h2>
 <?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "purple_z";
-
-$connection = mysqli_connect ($dbServername, $dbUsername, $dbPassword, $dbName);
-
-if(!$connection){
-    die("Error". mysqli_connect_error());
+$con = mysqli_connect("localhost","root","","purple_z");
+if(!$con){
+    die("Connection failed". mysqli_connect_error());
 }
-?>
+$qry= "SELECT * FROM product WHERE PROD_TYPE='Bag'";
+$rst=mysqli_query($con,$qry);
 
-<?php
-$sql = "SELECT * FROM product WHERE PROD_IMG = 'zahraa/images/sale';";
-$result  = mysqli_query($connection, $sql);
-$resultCheck = mysqli_num_rows($result);
-if ($resultCheck > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo $row ['3'];
-    }
+While($row=mysqli_fetch_assoc($rst)){
+    $prod_ID=$row["PROD_ID"];
+    $prod_NAME=$row["PROD_NAME"];
+    $prod_TYPE=$row["PROD_TYPE"];
+    $prod_IMG=$row["PROD_IMG"];
+    $prod_DESC=$row["PROD_DESC"];
+    $prod_STARS=$row["PROD_STARS"];
+    $prod_PRICE=$row["PROD_PRICE"];
+
+
+
+    ?>
+    <!--
+<img src="<?php echo $prod_IMG?>">
+<h1><?php echo $prod_NAME?></h1>
+<p><?php echo $prod_DESC?></p>
+<p class="price"><?php echo $prod_PRICE ?>
 }
-?>
